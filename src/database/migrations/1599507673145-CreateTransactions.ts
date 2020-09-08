acdaa2d089cd522cf15cf5d82260333ce5fc6d58
JOCILEUDO <jocileudosousav@gmail.com>
2020-09-07 22:20:00 -0300

    finished activity

---
+++ src/database/migrations/1599507673145-CreateTransactions.ts
@@ -0,0 +1,57 @@
+import {MigrationInterface, QueryRunner, Table, Timestamp} from "typeorm";
+
+export default class CreateTransactions1599507673145 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+      await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
+
+        await queryRunner.createTable(
+          new Table({
+              name: 'transactions',
+              columns: [
+                {
+                    name: 'id',
+                    type: 'uuid',
+                    isPrimary: true,
+                    generationStrategy: 'uuid',
+                    default: 'uuid_generate_v4()'
+                },
+                {
+                    name: 'title',
+                    type: 'varchar'
+                },
+                {
+                    name: 'type',
+                    type: 'varchar'
+                },
+                {
+                  name: 'value',
+                    type: 'decimal',
+                    precision: 10,
+                    scale: 2
+                },
+                {
+                  name: 'category',
+                  type: 'varchar',
+                },
+                {
+                  name: 'created_at',
+                  type: 'timestamp',
+                  default: 'now()' 
+                },
+                {
+                  name: 'updated_at',
+                  type: 'timestamp',
+                  default: 'now()' 
+                }
+              ]
+          })  
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('transactions');
+    }
+
+}
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,63 @@
-// import AppError from '../errors/AppError';
+import { getRepository, getCustomRepository } from 'typeorm';
 
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+import TransactionsRepository from "../repositories/TransactionsRepository";
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: "income" | "outcome";
+  category: string;
+} 
+
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({title, value, type, category}: Request): Promise<Transaction> {
+    
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Category);
+  
+    if(type != 'income' && type != 'outcome'){
+      throw new AppError("type is invaliable");
+    }
+    
+    if(typeof(category) == "undefined" || category == null) {
+      throw new AppError("Category is required");
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if(type == 'outcome' && total < value){
+      throw new AppError("You do not have enough balance");
+    }
+
+    //Verifica se existe já uma categoria
+    let transactionCategory = await categoryRepository.findOne({
+      where: {
+        title: category
+      }
+    });
+
+    // se não existe
+    if(!transactionCategory){
+        
+      transactionCategory = categoryRepository.create({ title: category });
+        
+        await categoryRepository.save(transactionCategory);
+    }
+    const transaction = transactionsRepository.create({
+      title, 
+      value, 
+      type,
+      category_id: transactionCategory.id
+    });
+    
+    await transactionsRepository.save(transaction);
+    
+    return transaction;
   }
 }
 
